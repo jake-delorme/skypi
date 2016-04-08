cffee3e9da659463fba2d202ca7535b1885fc4b7
Jake <Jake@Jakes-MacBook-Air.local>
2016-04-08 18:56:51 -0400

    a

--- daemon/Main.py
+++ daemon/Main.py
@@ -2,10 +2,28 @@
 
 import ConfigParser
 import os
-
+import sys
+import serial
 
 config = ConfigParser.SafeConfigParser()
 try:
-	config.readfp(os.path.dirname(__file__) + "config.ini")
-except ConfigParser.Error:
-	print "Caught an error YO"
+	config.readfp(open(os.path.dirname(os.path.abspath(__file__)) + "/../config.ini"))
+except IOError:
+	print "Cannot find configuration file " + os.path.dirname(os.path.abspath(__file__)) + "/../config.ini"
+	sys.exit(1)
+
+for section in config.sections():
+	for key in config.items(section):
+		print section + " - " + key[0] + " - " + key[1]
+
+
+try:
+	gsm = serial.Serial(
+		port=config.get('GSM','port'),
+		baudrate=config.get('GSM','baudrate'))
+except ValueError:
+	print "Invalid parameters for Serial port"
+	sys.exit(1)
+except serial.SerialException:
+	print "Cannot open serial port"
+	sys.exit(1)
