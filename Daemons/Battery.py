d2425894b8c1e4d2daba56525daef1e30e1c7614
Jake <jdelorme1@yahoo.com>
2017-03-20 20:21:30 -0400

    Thread starter on all modules

--- Daemons/Battery.py
+++ Daemons/Battery.py
@@ -10,3 +10,18 @@ class Battery(object):
 	def __init__(self):
 		# create the object yo
 		logging.debug("Create the Battery object")
+		self.name = "Battery"
+		self.listenerThread = threading.Thread(target=self.__listener, name=self.name+"-listener")
+		self.listenerThread.daemon = True
+		self.listenerThread.start()
+		self.consumerThread = threading.Thread(target=self.__queueConsumer, name=self.name+"-consumer")
+		self.consumerThread.daemon = True
+		self.consumerThread.start()
+
+	def __listener(self):
+		name = threading.current_thread().getName()
+		logging.debug("Running the "+name+" thread")
+		
+	def __queueConsumer(self):
+		name = threading.current_thread().getName()
+		logging.debug("Running the "+name+" thread")
--- Daemons/GPS.py
+++ Daemons/GPS.py
@@ -10,3 +10,18 @@ class GPS(object):
 	def __init__(self):
 		# create the object yo
 		logging.debug("Create the GPS object")
+		self.name = "GPS"
+		self.listenerThread = threading.Thread(target=self.__listener, name=self.name+"-listener")
+		self.listenerThread.daemon = True
+		self.listenerThread.start()
+		self.consumerThread = threading.Thread(target=self.__queueConsumer, name=self.name+"-consumer")
+		self.consumerThread.daemon = True
+		self.consumerThread.start()
+
+	def __listener(self):
+		name = threading.current_thread().getName()
+		logging.debug("Running the "+name+" thread")
+		
+	def __queueConsumer(self):
+		name = threading.current_thread().getName()
+		logging.debug("Running the "+name+" thread")
