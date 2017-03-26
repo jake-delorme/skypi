e6b44836aedbcc721d3cec1d0c3bd18b75336259
Jake <jdelorme1@yahoo.com>
2017-03-26 18:49:50 -0400

    gps testing

--- Daemons/skypi/GPS.py
+++ Daemons/skypi/GPS.py
@@ -103,4 +103,4 @@ class Gpslocation(object):
 		self.lastaltitudefix = 0
 
 	def getgoogleurl(self):
-		return 'https://maps.google.com/q=%s,%s' % (self.lattitude, self.longtitude)
+		return 'https://google.com/maps/place/%s,%s' % (self.lattitude, self.longtitude)
